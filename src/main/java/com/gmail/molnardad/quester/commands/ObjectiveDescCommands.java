080bce19e7c02585897800648a8a35d50b356f81
David Molnar <ragan.ragan@gmail.com>
2013-01-27 00:56:31 +0100

    added objective commands

---
+++ src/main/java/com/gmail/molnardad/quester/commands/ObjectiveDescCommands.java
@@ -0,0 +1,44 @@
+package com.gmail.molnardad.quester.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import com.gmail.molnardad.quester.QuestManager;
+import com.gmail.molnardad.quester.Quester;
+import com.gmail.molnardad.quester.commandbase.QCommand;
+import com.gmail.molnardad.quester.commandbase.QCommandContext;
+import com.gmail.molnardad.quester.commandbase.QCommandLabels;
+import com.gmail.molnardad.quester.exceptions.QuesterException;
+
+public class ObjectiveDescCommands {
+	
+	QuestManager qMan = null;
+	
+	public ObjectiveDescCommands(Quester plugin) {
+		qMan = QuestManager.getInstance();
+	}
+	
+	@QCommandLabels({"add", "a"})
+	@QCommand(
+			desc = "adds to objective description",
+			min = 2,
+			max = 2,
+			usage = "<objective ID> <description>")
+	public void add(QCommandContext context, CommandSender sender) throws QuesterException {
+		qMan.addObjectiveDescription(sender.getName(), context.getInt(0), context.getString(1));
+		sender.sendMessage(ChatColor.GREEN
+				+ context.getSenderLang().OBJ_DESC_ADD.replaceAll("%id", context.getString(0)));
+	}
+	
+	@QCommandLabels({"remove", "r"})
+	@QCommand(
+			desc = "removes objective description",
+			min = 1,
+			max = 1,
+			usage = "<objective ID>")
+	public void remove(QCommandContext context, CommandSender sender) throws QuesterException {
+		qMan.removeObjectiveDescription(sender.getName(), context.getInt(0));
+		sender.sendMessage(ChatColor.GREEN
+				+ context.getSenderLang().OBJ_DESC_REMOVE.replaceAll("%id", context.getString(0)));
+	}
+}
