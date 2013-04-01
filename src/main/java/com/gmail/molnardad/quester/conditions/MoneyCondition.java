ac74bea6b4cb44642f8823a6aaa97e8daa0525c8
David Molnar <ragan.ragan@gmail.com>
2013-04-01 17:22:39 +0200

    added -i flag to money condition, it causes inverted behavior

--- src/main/java/com/gmail/molnardad/quester/conditions/MoneyCondition.java
+++ src/main/java/com/gmail/molnardad/quester/conditions/MoneyCondition.java
@@ -14,9 +14,11 @@ import com.gmail.molnardad.quester.storage.StorageKey;
 public final class MoneyCondition extends Condition {
 
 	private final double amount;
+	private final boolean inverted;
 	
-	public MoneyCondition(double amount) {
+	private MoneyCondition(double amount, boolean invert) {
 		this.amount = amount;
+		this.inverted = invert;
 	}
 
 	@Override
@@ -24,7 +26,7 @@ public final class MoneyCondition extends Condition {
 		if(!Quester.vault) {
 			return true;
 		}
-		return Quester.econ.getBalance(player.getName()) >= amount;
+		return (Quester.econ.getBalance(player.getName()) >= amount) != inverted;
 	}
 	
 	@Override
@@ -35,7 +37,8 @@ public final class MoneyCondition extends Condition {
 	@Override
 	protected String show() {
 		if(Quester.vault) {
-			return "Must have " + amount + " " + Quester.econ.currencyNamePlural();
+			String flag = inverted ? "at least ": "less than ";
+			return "Must have " + flag + amount + " " + Quester.econ.currencyNamePlural() + ".";
 		}
 		else {
 			return "Money condition (Met)";
@@ -44,17 +47,18 @@ public final class MoneyCondition extends Condition {
 	
 	@Override
 	protected String info() {
-		return String.valueOf(amount);
+		String flag = inverted ? " (-i)": "";
+		return String.valueOf(amount) + flag;
 	}
 	
 	@QCommand(
 			min = 1,
 			max = 1,
-			usage = "<amount>")
+			usage = "<amount> (-i)")
 	public static Condition fromCommand(QCommandContext context) throws QCommandException {
 		try {
 			double amt = context.getDouble(1);
-			return new MoneyCondition(amt);
+			return new MoneyCondition(amt, context.hasFlag('i'));
 		}
 		catch (NumberFormatException e) {
 			throw new QCommandException(context.getSenderLang().ERROR_CMD_AMOUNT_GENERAL);
@@ -63,6 +67,9 @@ public final class MoneyCondition extends Condition {
 	
 	protected void save(StorageKey key) {
 		key.setDouble("amount", amount);
+		if(inverted) {
+			key.setBoolean("inverted", inverted);
+		}
 	}
 
 	protected static Condition load(StorageKey key) {
@@ -75,6 +82,6 @@ public final class MoneyCondition extends Condition {
 			return null;
 		}
 		
-		return new MoneyCondition(amt);
+		return new MoneyCondition(amt, key.getBoolean("inverted", false));
 	}
 }
