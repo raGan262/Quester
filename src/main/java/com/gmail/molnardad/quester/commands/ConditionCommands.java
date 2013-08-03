a620ec1bd3ad4600f8bc45b0f1459da80a293d71
raGan <ragan.ragan@gmail.com>
2013-08-03 19:08:11 +0200

    moved all condition commands to a single class

--- src/main/java/com/gmail/molnardad/quester/commands/ConditionCommands.java
+++ src/main/java/com/gmail/molnardad/quester/commands/ConditionCommands.java
@@ -113,4 +113,44 @@ public class ConditionCommands {
 		sender.sendMessage(ChatColor.RED + context.getSenderLang().CON_LIST + ": "
 				+ ChatColor.WHITE + eMan.getConditionList());
 	}
+	
+	public static class ConditionDescCommands {
+		
+		final QuestManager qMan;
+		final ProfileManager profMan;
+		
+		public ConditionDescCommands(final Quester plugin) {
+			qMan = plugin.getQuestManager();
+			profMan = plugin.getProfileManager();
+		}
+		
+		@QCommandLabels({ "add", "a" })
+		@QCommand(
+				section = "QMod",
+				desc = "adds condition description",
+				min = 2,
+				max = 2,
+				usage = "<condition ID> <description>")
+		public void add(final QCommandContext context, final CommandSender sender) throws QuesterException {
+			qMan.addConditionDescription(profMan.getProfile(sender.getName()), context.getInt(0),
+					context.getString(1), context.getSenderLang());
+			sender.sendMessage(ChatColor.GREEN
+					+ context.getSenderLang().CON_DESC_ADD.replaceAll("%id", context.getString(0)));
+		}
+		
+		@QCommandLabels({ "remove", "r" })
+		@QCommand(
+				section = "QMod",
+				desc = "adds to condition description",
+				min = 1,
+				max = 1,
+				usage = "<condition ID>")
+		public void remove(final QCommandContext context, final CommandSender sender) throws QuesterException {
+			qMan.removeConditionDescription(profMan.getProfile(sender.getName()),
+					context.getInt(0), context.getSenderLang());
+			sender.sendMessage(ChatColor.GREEN
+					+ context.getSenderLang().CON_DESC_REMOVE.replaceAll("%id",
+							context.getString(0)));
+		}
+	}
 }
