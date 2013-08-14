c3ea4f1fe89ab671115a5e4f7f4e08f25c802258
raGan <ragan.ragan@gmail.com>
2013-08-15 00:38:17 +0200

    changed QUEST event to use quest names instead of IDs

--- src/main/java/com/gmail/molnardad/quester/qevents/QuestQevent.java
+++ src/main/java/com/gmail/molnardad/quester/qevents/QuestQevent.java
@@ -1,5 +1,6 @@
 package com.gmail.molnardad.quester.qevents;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -10,29 +11,52 @@ import com.gmail.molnardad.quester.commandbase.QCommandContext;
 import com.gmail.molnardad.quester.commandbase.exceptions.QCommandException;
 import com.gmail.molnardad.quester.elements.QElement;
 import com.gmail.molnardad.quester.elements.Qevent;
+import com.gmail.molnardad.quester.exceptions.QuestException;
 import com.gmail.molnardad.quester.exceptions.QuesterException;
+import com.gmail.molnardad.quester.lang.LanguageManager;
+import com.gmail.molnardad.quester.quests.Quest;
+import com.gmail.molnardad.quester.quests.QuestManager;
 import com.gmail.molnardad.quester.storage.StorageKey;
 import com.gmail.molnardad.quester.utils.Ql;
 
 @QElement("QUEST")
 public final class QuestQevent extends Qevent {
 	
-	private final int quest;
+	private final String quest;
 	
-	public QuestQevent(final int qst) {
+	public QuestQevent(final String qst) {
 		quest = qst;
 	}
 	
 	@Override
 	public String info() {
-		return String.valueOf(quest);
+		return quest;
 	}
 	
 	@Override
 	protected void run(final Player player, final Quester plugin) {
+		final QuestManager qm = plugin.getQuestManager();
+		String warning = null;
+		Quest q = null;
 		try {
+			q = qm.getQuest(quest);
+		}
+		catch (final Exception ignore) {}
+		try {
+			if(q == null) {
+				try {
+					q = qm.getQuest(Integer.valueOf(quest));
+					warning = "Deprecated usage of Quest ID in QUEST event detected.";
+				}
+				catch (final NumberFormatException e) {
+					throw new QuestException(LanguageManager.defaultLang.get("ERROR_Q_NOT_EXIST"));
+				}
+			}
 			plugin.getProfileManager().startQuest(player, quest, ActionSource.eventSource(this),
-					plugin.getLanguageManager().getPlayerLang(player.getName()));
+					LanguageManager.defaultLang);
+			if(warning != null) {
+				Ql.warning(warning);
+			}
 		}
 		catch (final QuesterException e) {
 			Ql.warning("Event failed to give quest to " + player.getName() + ". Reason: "
@@ -40,23 +64,26 @@ public final class QuestQevent extends Qevent {
 		}
 	}
 	
-	@QCommand(min = 1, max = 1, usage = "<quest ID>")
+	@QCommand(min = 1, max = 1, usage = "<quest name>")
 	public static Qevent fromCommand(final QCommandContext context) throws QCommandException {
-		final int id = context.getInt(0);
-		if(id < 0) {
-			throw new QCommandException(context.getSenderLang().get("ERROR_CMD_BAD_ID"));
+		final Quester plugin = (Quester) Bukkit.getPluginManager().getPlugin("Quester");
+		
+		if(plugin.getQuestManager().getQuest(context.getString(0)) == null) {
+			
+			context.getSender().sendMessage(
+					ChatColor.YELLOW + context.getSenderLang().get("ERROR_Q_NOT_EXIST"));
 		}
-		return new QuestQevent(id);
+		return new QuestQevent(context.getString(0));
 	}
 	
 	@Override
 	protected void save(final StorageKey key) {
-		key.setInt("quest", quest);
+		key.setString("quest", quest);
 	}
 	
 	protected static Qevent load(final StorageKey key) {
-		final int qst = key.getInt("quest", -1);
-		if(qst < 0) {
+		final String qst = key.getString("quest");
+		if(qst == null) {
 			return null;
 		}
 		
